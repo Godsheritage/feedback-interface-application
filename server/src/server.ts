63863b4e48c5ee7a505781ca217b6f20878aa95b
Godsheritage Adeoye <adeoyegodsheritage@gmail.com>
2022-03-31 18:59:40 +0100

    seperating the server code from the express middleware

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,27 +1,11 @@
-import express from 'express'
-import feedbackRouter from './Routes/feedback.routes'
-import path from 'path';
-const app = express();
+import http from 'http'
+import app from './app';
 
 const PORT = process.env.PORT || 5000;
-// seperate the server code from the express middleware then deploy 
 
-app.use((req, res, next) => {
-  res.append('Access-Control-Allow-Origin', ['*'])
-  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
-  res.append('Access-Control-Allow-Headers', 'Content-Type')
-  next()
-});
+const server = http.createServer(app)
 
-app.use(express.json());
-
-app.use('/feedback', feedbackRouter);
-
-app.use(express.static(path.join(__dirname, "..", "public ")));
-
-app.get("/*", (req , res) => {
-  res.sendFile(path.join(__dirname, "..", "public ", "index.html"));
-});
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running at port ${PORT}...`)
 });
+
