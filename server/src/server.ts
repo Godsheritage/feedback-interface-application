8a2918211048287cec36deb7e1fbc78402f526ae
Godsheritage Adeoye <adeoyegodsheritage@gmail.com>
2022-04-03 16:42:07 +0100

    updates

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,17 +4,16 @@ import mongoose from "mongoose";
 
 const PORT = process.env.PORT || 5000;
 // mongo Url from the feedback cluster
-const MONGO_URL =
-  "mongodb+srv://feedback-api:Heritage4lyf@feedbackcluster.pweoe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const MONGO_URL = "mongodb+srv://feedback-api:Heritage4lyf@feedbackcluster.pweoe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 const server = http.createServer(app);
-
 
 // to alert us when mongoose has connected to mongo db
 mongoose.connection.once("open", () => {
   console.log("MongoDB Connection is ready");
 });
 
+// mongoose error handler
 mongoose.connection.on("eror", (err) => {
   console.error(err);
 });
