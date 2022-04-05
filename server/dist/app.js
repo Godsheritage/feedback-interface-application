4779738097dbdc0517b50df0faf7164df24c2f7f
Godsheritage Adeoye <adeoyegodsheritage@gmail.com>
2022-04-05 11:29:25 +0100

    deploying to heroku

--- server/dist/app.js
+++ server/dist/app.js
@@ -12,15 +12,19 @@ const morgan_1 = __importDefault(require("morgan"));
 const app = (0, express_1.default)();
 // to enable the Cross-Origin Resource Sharing
 app.use((0, cors_1.default)());
-app.use((0, morgan_1.default)('combined'));
+app.use((0, morgan_1.default)("combined"));
 // to setup th e express aplication
 app.use(express_1.default.json());
 // to setup the feedback Route
 app.use("/feedback", feedback_routes_1.default);
-//to serve the front end with the back end
-app.use(express_1.default.static(path_1.default.join(__dirname, "..", "public ")));
-// to get the index.html page
-app.get("/*", (req, res) => {
-    res.sendFile(path_1.default.join(__dirname, "..", "public ", "index.html"));
-});
+//serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+    // set static folder
+    //to serve the front end with the back end
+    app.use(express_1.default.static(path_1.default.resolve(__dirname, "..", "public ")));
+    // to get the index.html page
+    app.get("/*", (req, res) => {
+        res.sendFile(path_1.default.resolve(__dirname, "..", "public ", "index.html"));
+    });
+}
 exports.default = app;
--- server/src/app.ts
+++ server/src/app.ts
@@ -2,7 +2,7 @@ import express from "express";
 import feedbackRouter from "./Routes/feedback.routes";
 import path from "path";
 import cors from "cors";
-import morgan from 'morgan'
+import morgan from "morgan";
 
 // setup the express application
 const app = express();
@@ -10,7 +10,7 @@ const app = express();
 // to enable the Cross-Origin Resource Sharing
 app.use(cors());
 
-app.use(morgan('combined'))
+app.use(morgan("combined"));
 
 // to setup th e express aplication
 app.use(express.json());
@@ -18,12 +18,18 @@ app.use(express.json());
 // to setup the feedback Route
 app.use("/feedback", feedbackRouter);
 
-//to serve the front end with the back end
-app.use(express.static(path.join(__dirname, "..", "public ")));
+//serve static assets if in production
 
-// to get the index.html page
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "public ", "index.html"));
-});
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  
+  //to serve the front end with the back end
+  app.use(express.static(path.resolve(__dirname, "..", "public ")));
+
+  // to get the index.html page
+  app.get("/*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "..", "public ", "index.html"));
+  });
+}
 
 export default app;
